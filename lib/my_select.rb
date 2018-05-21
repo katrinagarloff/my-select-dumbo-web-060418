0abf9e487959676504df28fdfd9b9a2b48208241
Katrina Garloff <katrinagarloff@gmail.com>
2018-05-21 22:22:44 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,8 @@ def my_select(collection)
 i = 0 
 new_collection = []
 while i < collection.length 
-if yield collection[i]
+if yield collection[i] == true
+  new_collection << collection[i]
 i+=1
 end
 new_collection
